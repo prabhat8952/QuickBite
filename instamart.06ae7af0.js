1a8cff35308c31fd4ae538af9c2d8ba1fbe85759
Prabhat Kumar <PRA854109@GMAIL.COM>
2024-03-11 18:58:30 +0530

    Updates

---
+++ instamart.06ae7af0.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire94c2;(0,e.register)("470mr",function(r,t){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>i,set:void 0,enumerable:!0,configurable:!0});var a=e("ayMG0"),l=e("aphAM"),i=()=>(0,a.jsxs)(a.Fragment,{children:[(0,a.jsx)(l.Outlet,{}),(0,a.jsx)("h1",{children:"Instamart"})]})});
+//# sourceMappingURL=instamart.06ae7af0.js.map
